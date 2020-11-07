86435a2f9d560d287231dcfab310bdeded4d4207
Yaroslav <boyaal24@gmail.com>
2020-11-07 19:58:46 +0200

    add function to project

---
+++ script.js
@@ -0,0 +1,55 @@
+'use strict';
+
+/* console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Correct Number! 🥇🥈🥉';
+
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.number').textContent = '23';
+document.querySelector('.score').textContent = '100';
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value); */
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+let score = 20;
+
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  // When player wins
+  if (!guess) {
+    document.querySelector('.message').textContent = 'Wrong Number!😢';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent =
+      '✌🎉 You guess a  Number!';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    // When guess is too high
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too high!📈';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+
+    // When guess is too low
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too low!📉';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
